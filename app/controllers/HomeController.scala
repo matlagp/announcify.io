16fcb29529277295604cd6be95f04031c1ed6ebd
Bartek Łagosz <lagosz.bartlomiej@gmail.com>
2019-06-08 13:20:35 +0100

    commit

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,14 +1,19 @@
 package controllers
 
 import javax.inject._
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.mvc._
+import slick.jdbc.JdbcProfile
+import scala.concurrent.ExecutionContext
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(protected val dbConfigProvider: DatabaseConfigProvider,
+                               cc: ControllerComponents)(implicit ec: ExecutionContext)
+  extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
 
   /**
    * Create an Action to render an HTML page with a welcome message.
--- build.sbt
+++ build.sbt
@@ -10,7 +10,8 @@ resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
       
 scalaVersion := "2.12.2"
 
-libraryDependencies ++= Seq( jdbc , ehcache , ws , specs2 % Test , guice )
+libraryDependencies ++= Seq( ehcache , ws , specs2 % Test , guice,"com.typesafe.play" %% "play-slick" % "3.0.0",
+"com.typesafe.play" %% "play-slick-evolutions" % "3.0.0","org.postgresql" % "postgresql" % "9.4-1201-jdbc41")
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
 
